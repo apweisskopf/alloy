ebba19a9bfed6059634ec82882a7c101077a615c
Kalle <23356117+kalleep@users.noreply.github.com>
2026-03-06 10:59:08 +0100

    move interface to own file

---
+++ internal/component/loki/source/internal/positions/positions.go
@@ -0,0 +1,26 @@
+package positions
+
+import "time"
+
+type Positions interface {
+	// GetString returns how far we've through a file as a string.
+	// JournalTarget writes a journal cursor to the positions file, while
+	// FileTarget writes an integer offset. Use Get to read the integer
+	// offset.
+	GetString(path, labels string) string
+	// Get returns how far we've read through a file. Returns an error
+	// if the value stored for the file is not an integer.
+	Get(path, labels string) (int64, error)
+	// PutString records (asynchronously) how far we've read through a file.
+	// Unlike Put, it records a string offset and is only useful for
+	// JournalTargets which doesn't have integer offsets.
+	PutString(path, labels string, pos string)
+	// Put records (asynchronously) how far we've read through a file.
+	Put(path, labels string, pos int64)
+	// Remove removes the position tracking for a filepath
+	Remove(path, labels string)
+	// SyncPeriod returns how often the positions file gets resynced
+	SyncPeriod() time.Duration
+	// Stop the Position tracker.
+	Stop()
+}
